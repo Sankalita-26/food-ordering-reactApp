1916266d06e4694bfd19458e00080c4b27f70562
Sankalita Nandan <sankalitanandan@Sankalitas-MacBook-Air.local>
2025-12-06 09:45:00 +0530

    updated API

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -7,7 +7,7 @@ export const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtB
 
 export const FOODFIRE_API_URL = "https://namastedev.com/api/v1/listRestaurants";
 
-export const Menu_API ="https://corsproxy.io/https://namastedev.com/api/v1/listRestaurantMenu";
+export const Menu_API ="https://corsproxy.io/https://namastedev.com/api/v1/listRestaurantMenu/";
 
     
 export const Restaurant_API="https://corsproxy.io/https://namastedev.com/api/v1/listRestaurants?lat=17.385044&lng=78.486671";
